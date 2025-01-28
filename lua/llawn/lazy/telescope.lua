a0d52a64d7896912792b964e97c08c5c0e13f9a6
llawn <llawn06@gmail.com>
2025-01-28 19:23:01 +0100

    feat(telescope): change telescope remap

--- lua/llawn/lazy/telescope.lua
+++ lua/llawn/lazy/telescope.lua
@@ -13,29 +13,29 @@ return {
         local builtin = require('telescope.builtin')
 
 	-- Search for files in the current project (find_files)
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
 	-- Search for files tracked by Git (git_files)
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 
 	-- Search the current word under the cursor
-        vim.keymap.set('n', '<leader>pws', function()
+        vim.keymap.set('n', '<leader>fw', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
 
 	-- Search the current WORD (including punctuation)
-        vim.keymap.set('n', '<leader>pWs', function()
+        vim.keymap.set('n', '<leader>fW', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
 
 	-- Prompt for a search term and perform a grep search in a project
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set('n', '<leader>gs', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         
 	-- Open help tags search
-	vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+	vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 end
 }
